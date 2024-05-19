26105984bc5433fcc488241cd3b38aef8bcd6600
Kudrixon <35082391+Kudrixon@users.noreply.github.com>
2024-05-19 02:49:51 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',
-  neo4j.auth.basic(process.env.NEO4J_USER || 'neo4j', process.env.NEO4J_PASSWORD || 'Polska_30')
+  neo4j.auth.basic(process.env.NEO4J_USER || 'neo4j', process.env.NEO4J_PASSWORD || 'password')
 );
 
 const clearDatabase = async () => {
